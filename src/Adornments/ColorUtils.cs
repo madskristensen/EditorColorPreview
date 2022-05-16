210016e08e14fc0060e143bdd53878fb3855f3ad
Guy Boicey <guy@boicey.net>
2022-05-15 21:47:20 -0700

    Add support for css-color-4
    1. Add HSL and HWB using the W3 as a reference
        a. https://www.w3.org/TR/css-color-4/#hsl-to-rgb
        b. https://www.w3.org/TR/css-color-4/#hwb-to-rgb
    2. Changed RGBA parts parsing to handle spaces, comas, %, deg, slash, none
    4. Move the RegEx and the Matching logic into ColorUtils which is exposed as publish and allows for unit testing
    5. Added to the RegEx for matching with hwb, spaces without commas, slashes for alpha channel
    4. Improved number accuracy by rounding and added clamping to keep numbers within range

--- src/Adornments/ColorUtils.cs
+++ src/Adornments/ColorUtils.cs
@@ -1,10 +1,13 @@
 ﻿using System.Drawing;
 using System.Globalization;
-using Microsoft.VisualStudio.Imaging;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 // From https://stackoverflow.com/a/61871235
 public static class ColorUtils
 {
+    public static readonly Regex _regex = new(@"(?<=.*:.*)(#(?:[0-9a-f]{2}){2,4}\b|(#[0-9a-f]{3})\b|\b(rgb|hsl|hwb)a?\(((\d+(%|deg)?|none)(,|,\s*|\s*)){2}(\d+(%|deg)?|none){1}\s*[\/\d\.]*%?\s*([\d\.]*)*\)|(?<=[\s""'])(black|silver|gray|whitesmoke|maroon|red|purple|fuchsia|green|lime|olivedrab|yellow|navy|blue|teal|aquamarine|orange|aliceblue|antiquewhite|aqua|azure|beige|bisque|blanchedalmond|blueviolet|brown|burlywood|cadetblue|chartreuse|chocolate|coral|cornflowerblue|cornsilk|crimson|darkblue|darkcyan|darkgoldenrod|darkgray|darkgreen|darkgrey|darkkhaki|darkmagenta|darkolivegreen|darkorange|darkorchid|darkred|darksalmon|darkseagreen|darkslateblue|darkslategray|darkslategrey|darkturquoise|darkviolet|deeppink|deepskyblue|dimgray|dimgrey|dodgerblue|firebrick|floralwhite|forestgreen|gainsboro|ghostwhite|goldenrod|gold|greenyellow|grey|honeydew|hotpink|indianred|indigo|ivory|khaki|lavenderblush|lavender|lawngreen|lemonchiffon|lightblue|lightcoral|lightcyan|lightgoldenrodyellow|lightgray|lightgreen|lightgrey|lightpink|lightsalmon|lightseagreen|lightskyblue|lightslategray|lightslategrey|lightsteelblue|lightyellow|limegreen|linen|mediumaquamarine|mediumblue|mediumorchid|mediumpurple|mediumseagreen|mediumslateblue|mediumspringgreen|mediumturquoise|mediumvioletred|midnightblue|mintcream|mistyrose|moccasin|navajowhite|oldlace|olive|orangered|orchid|palegoldenrod|palegreen|paleturquoise|palevioletred|papayawhip|peachpuff|peru|pink|plum|powderblue|rosybrown|royalblue|saddlebrown|salmon|sandybrown|seagreen|seashell|sienna|skyblue|slateblue|slategray|slategrey|snow|springgreen|steelblue|tan|thistle|tomato|transparent|turquoise|violet|wheat|white|yellowgreen|rebeccapurple))", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
     public static Color HtmlToColor(string htmlColor)
     {
         try
@@ -19,6 +22,12 @@ public static class ColorUtils
             if (htmlColor.StartsWith("hsl", StringComparison.OrdinalIgnoreCase))
             {
                 return HslToColor(htmlColor);
+            }
+
+            // HWB
+            if (htmlColor.StartsWith("hwb", StringComparison.OrdinalIgnoreCase))
+            {
+                return HwbToColor(htmlColor);
             }
 
             // HEX
@@ -40,6 +49,11 @@ public static class ColorUtils
         {
             return Color.Empty;
         }
+    }
+
+    public static MatchCollection MatchesColor(string html)
+    {
+        return _regex.Matches(html);
     }
 
     private static Color HexToColor(string htmlColor)
@@ -91,77 +105,167 @@ public static class ColorUtils
 
     private static Color ArgbToColor(string htmlColor)
     {
-        int left = htmlColor.IndexOf('(');
-        int right = htmlColor.IndexOf(')');
+        string[] parts = ColorParts(htmlColor);
 
-        if (left < 0 || right < 0)
+        double r = GetNumberOrPercentage(parts[0]);
+        double g = GetNumberOrPercentage(parts[1]);
+        double b = GetNumberOrPercentage(parts[2]);
+
+        double a = parts.Length > 3 ? GetNumberOrPercentage(parts[3]) : 1.0;
+
+        if (parts[0].Contains('%') || parts[1].Contains('%') || parts[2].Contains('%'))
         {
-            return Color.Empty;
+            return FromArgb(a, r, g, b);
         }
+        else
+        {
+            return FromArgb(a, (int)r, (int)g, (int)b);
+        }
+    }
 
-        string noBrackets = htmlColor.Substring(left + 1, right - left - 1);
+    private static Color HslToColor(string htmlColor)
+    {
+        string[] parts = ColorParts(htmlColor);
 
-        string[] parts = noBrackets.Split(',');
+        double h = GetNumberOrPercentage(parts[0]);
+        double s = GetNumberOrPercentage(parts[1]);
+        double l = GetNumberOrPercentage(parts[2]);
 
-        int r = int.Parse(parts[0], CultureInfo.InvariantCulture);
-        int g = int.Parse(parts[1], CultureInfo.InvariantCulture);
-        int b = int.Parse(parts[2], CultureInfo.InvariantCulture);
+        double[] rgb = HslToRgb(h, s, l);
 
         if (parts.Length == 3)
         {
-            return Color.FromArgb(r, g, b);
+            return FromArgb(rgb[0], rgb[1], rgb[2]);
         }
         else if (parts.Length == 4)
         {
             float a = float.Parse(parts[3], CultureInfo.InvariantCulture);
 
-            return Color.FromArgb((int)(a * 255), r, g, b);
+            return FromArgb(a, rgb[0], rgb[1], rgb[2]);
         }
 
         return Color.Empty;
     }
 
-    private static Color HslToColor(string htmlColor)
+    private static Color HwbToColor(string htmlColor)
+    {
+        string[] parts = ColorParts(htmlColor);
+
+        double hue = GetNumberOrPercentage(parts[0]);
+        double white = GetNumberOrPercentage(parts[1]);
+        double black = GetNumberOrPercentage(parts[2]);
+        
+        double a = parts.Length > 3 ? GetNumberOrPercentage(parts[3]) : 1.0;
+
+        if (white + black >= 1)
+        {
+            double gray = white / (white + black);
+            return FromArgb(a, gray, gray, gray);
+        }
+
+        double[] rgb = HslToRgb(hue, 1, .5);
+        float[] rgb2 = HslToRgb(hue, 1, .5).Select(v => (float)v).ToArray();
+
+        for (int i = 0; i < 3; i++)
+        {
+            rgb[i] *= Math.Round((1 - white - black), 2);
+            rgb[i] += white;
+        }
+
+        return FromArgb(a, rgb[0], rgb[1], rgb[2]);
+    }
+
+    private static double[] HslToRgb(double hue, double sat, double light)
+    {
+        hue %= 360.0;
+
+        if (hue < 0)
+        {
+            hue += 360;
+        }
+
+        double f(double n)
+        {
+            double k = (n + hue / 30.0) % 12.0;
+            double a = sat * Math.Min(light, 1 - light);
+            return light - a * Math.Max(-1, Math.Min(Math.Min(k - 3.0, 9.0 - k), 1));
+        };
+
+        return new double[] { f(0), f(8), f(4) };
+    }
+
+    private static string[] ColorParts(string htmlColor)
     {
         int left = htmlColor.IndexOf('(');
         int right = htmlColor.IndexOf(')');
 
         if (left < 0 || right < 0)
         {
-            return Color.Empty;
+            return new string[] { };
         }
 
-        string noBrackets = htmlColor.Substring(left + 1, right - left - 1);
+        string noBrackets = htmlColor.Substring(left + 1, right - left - 1).Replace("deg", "");
 
-        string[] parts = noBrackets.Split(',');
+        char seperator = htmlColor.Contains(',') ? ',' : ' ';
 
-        double h = GetDouble(parts[0], false);
-        double s = GetDouble(parts[1]);
-        double l = GetDouble(parts[2]);
-        double alpha = 1.0;
-
-        System.Windows.Media.Color hsl = new HslColor(h, s, l).ToColor();
-
-        if (parts.Length == 4)
+        string[] parts;
+        string[] alphaChannel = noBrackets.Split('/');
+        if (alphaChannel.Length == 2)
         {
-            alpha = GetDouble(parts[3]);
+            parts = alphaChannel[0].Trim().Split(seperator).Append(alphaChannel[1]).ToArray();
+        }
+        else
+        {
+            parts = alphaChannel[0].Split(seperator);
         }
 
-        return Color.FromArgb((int)(alpha * 255), hsl.R, hsl.G, hsl.B);
+        return parts.Select(p => p.Trim() == "none" ? "0" : p.Trim()).ToArray(); ;
     }
 
-    private static double GetDouble(string part, bool divide = true)
+    private static double GetNumberOrPercentage(string part)
     {
         string raw = part.TrimEnd('%');
 
         if (double.TryParse(raw, out double d))
         {
-            if (divide && d > 1)
+            if (part.Contains('%') && d > 1)
             {
-                return d / 100;
+                return d / 100.0;
             }
         }
 
         return d;
+    }
+
+    public static Color FromArgb(double a, int r, int g, int b)
+    {
+        a = Math.Round(ClampAlpha(a) * 255.0, MidpointRounding.AwayFromZero);
+        return Color.FromArgb((int)a, ClampRgb(r), ClampRgb(g), ClampRgb(b));
+    }
+
+    public static Color FromArgb(double r, double g, double b)
+    {
+        return FromArgb(1.0, r, g, b);
+    }
+
+    public static Color FromArgb(double a, double r, double g, double b)
+    {
+        a = Math.Round(ClampAlpha(a) * 255.0, MidpointRounding.AwayFromZero);
+        r = Math.Round(r * 255.0, MidpointRounding.AwayFromZero);
+        g = Math.Round(g * 255.0, MidpointRounding.AwayFromZero);
+        b = Math.Round(b * 255.0, MidpointRounding.AwayFromZero);
+        return Color.FromArgb(ClampRgb((int)a), ClampRgb((int)r), ClampRgb((int)g), ClampRgb((int)b));
+    }
+
+    public static int ClampRgb(int value)
+    {
+        return value < 0 ? 0 : value > 255 ? 255 : value;
+        
+    }
+
+    public static double ClampAlpha(double value)
+    {
+        return value < 0.0 ? 0.0 : value > 1.0 ? 1.0 : value;
+
     }
 }
